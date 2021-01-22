4c65eb082a30182593d0b0a96b0d9a26a27620fe
Zoe Greene <zoe.greene12@gmail.com>
2021-01-21 20:53:29 -0500

    troubleshootl ssl

--- server/db/db.js
+++ server/db/db.js
@@ -3,9 +3,12 @@ pg.defaults.ssl = true;
 
 const Sequelize = require('sequelize');
 
-const db = new Sequelize (
-  process.env.DATABASE_URL || 'postgres://localhost:5432/sequence_aligner',
-  { logging: false }
-);
+const db = new Sequelize ({
+  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/sequence_aligner',
+  ssl: {
+    rejectUnauthorized: false
+  }
+  // { logging: false }
+});
 
 module.exports = db;
