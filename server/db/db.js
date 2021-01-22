058f38c1d311f9a8aced29264732fac37b5a7407
Zoe Greene <zoe.greene12@gmail.com>
2021-01-21 21:04:57 -0500

    reverse course

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,10 @@
-const pg = require('pg');
-pg.defaults.ssl = true;
-
 const Sequelize = require('sequelize');
 
-const db = new Sequelize ({
-  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/sequence_aligner',
-  ssl: {
-    rejectUnauthorized: false
+const db = new Sequelize (
+  process.env.DATABASE_URL || 'postgres://localhost:5432/sequence_aligner', {
+    logging: false,
+    operatorsAliases: false
   }
-  // { logging: false }
-});
+);
 
 module.exports = db;
