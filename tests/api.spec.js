1040f718dbbf23fe217ed6840be1cc93d99aa6ec
Zoe Greene <zoe.greene12@gmail.com>
2021-01-30 12:13:29 -0500

    tests for all used routes

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,23 +1,22 @@
-const { test, expect } = require('@jest/globals');
+const { expect } = require('@jest/globals');
 const request = require('supertest');
 const app = require('../server/index');
-
-let sessionId;
 
 describe('Create a new session ID', () => {
   test('It should response the POST method with 201 and data', async () => {
     const response = await request(app).post('/api/sessions');
     expect(response.statusCode).toBe(201);
-    sessionId = response.body.id;
-    console.log('session in first post', sessionId);
     expect(response.body.id).toBeTruthy;
   });
 });
 
 describe('Create a new alignment', () => {
   let response;
-  console.log('session in post',sessionId);
+  let sessionId;
+
   test('It should response the POST method with 201', async () => {
+    sessionId = await request(app).get('/api/sessions');
+    sessionId = sessionId.body[sessionId.body.length - 1].id;
     const data = {
       seq1: 'abc',
       seq2: 'abd',
@@ -25,22 +24,16 @@ describe('Create a new alignment', () => {
       match: 0.67,
       numMutations: 1
     };
-    response =  await request(app).post(`/api/sessions/${sessionId}/alignments`).send(data);
+    response = await request(app).post(`/api/sessions/${sessionId}/alignments`).send(data);
     expect(response.statusCode).toBe(201);
   });
   it('Adds the passed in data for the given session Id', () => {
     expect(response.body.newSeq).toBe('ab[c]');
     expect(response.body.sessionId).toBe(sessionId);
   });
+  test('It should response the GET method with 200 and array of alignments', async () => {
+    const getResponse = await request(app).get(`/api/sessions/${sessionId}/alignments`);
+    expect(getResponse.statusCode).toBe(200);
+    expect(getResponse.body.length).toBe(1);
+  });
 });
-
-// GET /api/sessions/:sessionId/alignments
-// Retrieves the alignments history of a given sessionId.
-// describe('Get all alignments for a give sessionId', () => {
-//   let response;
-//   console.log(sessionId);
-//   test('It should response the GET method with 200', async () => {
-//     response = await request(app).get(`api/sessions/${sessionId}/alignments`);
-//     expect(response.statusCode).toBe(200);
-//   });
-// })
